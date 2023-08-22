64d11a92559a144c8a0b49c9d4a8a91cbc0ba61f
Evgen <makeevgen1975@gmail.com>
2023-08-22 09:24:52 +0300

    add veb002 if square

---
+++ veb002_if_square/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string input1 = Console.ReadLine();
+Console.Write("Введите число: ");
+string input2 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+int number2 = Convert.ToInt32(input2);
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
