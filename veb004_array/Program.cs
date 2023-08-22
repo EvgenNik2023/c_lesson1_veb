62f2f576d5f80186e0a0abd6636156d9f7d9166a
Evgen <makeevgen1975@gmail.com>
2023-08-22 15:45:54 +0300

    add veb004 array and veb005 last digit

---
+++ veb004_array/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int number1 = 0 - number;
+Console.Write("\"");
+while (number1 < number)
+{
+    Console.Write(number1 + ", ");
+    number1++;
+}
+Console.WriteLine(number + "\"");
---
+++ veb005_last_digit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int digit = number % 10;
+Console.WriteLine(digit);
